0b313cd588be46f6354610c935ddce064b93aa7e
shouzhuoyi <3175882821@qq.com>
2026-03-30 17:40:47 +0800

    faster

--- sources_1/new/clk_div.v
+++ sources_1/new/clk_div.v
@@ -9,6 +9,6 @@ module clk_div(input clk,
 	always @ (posedge clk or posedge rst) begin 
 		if (rst) clkdiv <= 0; else clkdiv <= clkdiv + 1'b1; end
 		
-	assign Clk_CPU=(SW2)? clkdiv[24] : clkdiv[3];
+	assign Clk_CPU=(SW2)? clkdiv[1] : clkdiv[0];
 		
 endmodule
